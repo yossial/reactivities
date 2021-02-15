b90808ce35760356bec00bca88b2aae28eccaba9
yossi a <yossi.alfia@gmail.com>
2021-02-16 00:40:14 +0200

    Created project structure

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+  public class ActivitiesController : BaseApiController
+  {
+    private readonly DataContext _dataContext;
+    public ActivitiesController(DataContext dataContext)
+    {
+      _dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+      return await _dataContext.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+      return await _dataContext.Activities.FindAsync(id);
+    }
+
+  }
+}
---
+++ Persistence/Migrations/20210215221850_UpdateDateCol.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistene.Migrations
+{
+    public partial class UpdateDateCol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "date",
+                table: "Activities",
+                newName: "Date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Activities",
+                newName: "date");
+        }
+    }
+}
