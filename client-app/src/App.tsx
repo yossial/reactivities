72dcd158c3f55c315b1808f61b346cc3f2314ca0
yossi a <yossi.alfia@gmail.com>
2021-02-16 02:00:45 +0200

    Added Material UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { createStyles, List, ListItem, makeStyles, Theme } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+  }),
+);
 
 function App() {
-
+  <CssBaseline />
+  const classes = useStyles();
   const [activities,setActivities] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:5000/api/activities").then(response=>{
@@ -13,17 +26,16 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <ul>
-       {activities.map((activity : any)=>(
-          <li key={activity.id}>
-            {activity.title}
-          </li>
-       ))
-      }
-       </ul>
-      </header>
+      <div className={classes.root}>
+          <List component="nav" aria-label="activities">
+          {activities.map((activity : any)=>(
+              <ListItem key={activity.id}>
+                 {activity.title}
+              </ListItem>
+          ))
+          }
+          </List>
+        </div>
     </div>
   );
 }
