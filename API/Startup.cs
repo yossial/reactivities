650ea8cf210f24f620761d62843b4196ae165e07
yossi a <yossi.alfia@gmail.com>
2021-02-16 21:58:40 +0200

    Moved services registration to Extention class

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,6 @@
+using API.Extentions;
 using Application.Activities;
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -23,25 +25,8 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-
       services.AddControllers();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-      });
-      services.AddDbContext<DataContext>(opt =>
-      {
-        opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-      });
-      services.AddCors(opt =>
-      {
-        opt.AddPolicy("CorsPolicy", policy =>
-        {
-          policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-        });
-      });
-
-      services.AddMediatR(typeof(List.Handler).Assembly);
+      services.AddApplicationServices(_config);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
